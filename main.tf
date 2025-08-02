72d46fd2c741ec79db13f876cb1673ed001019a6
Vincent Mahnke <vincent@mahn.ke>
2025-08-02 03:58:55 +0200

    fix: Fetch subdomain from parent directory name

--- main.tf
+++ main.tf
@@ -18,10 +18,14 @@ resource "random_password" "ddns_password" {
     special = false
 }
 
-resource "allinkl_ddns" "ttrss" {
-    dyndns_comment   = "ttrss new NOT OLD"
+locals {
+  subdomain_label = replace(replace(basename(dirname(path.cwd)), "-by-vincent", ""), "-", ".")
+}
+
+resource "allinkl_ddns" "subdomain" {
+    dyndns_comment   = local.subdomain_label
     dyndns_password  = random_password.ddns_password.result
     dyndns_zone      = "mahn.ke"
-    dyndns_label     = "ttrssold.by.vincent"
+    dyndns_label     = "${local.subdomain_label}.by.vincent"
     dyndns_target_ip = "88.99.215.101"
 }
