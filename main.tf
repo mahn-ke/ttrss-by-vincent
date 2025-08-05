305275ce2760130a66bbf27ba9c1eca084e33817
Vincent Mahnke <vincent@mahn.ke>
2025-08-05 11:55:42 +0000

    feat(terraform): Adds Keycloak OpenAPI client

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ terraform {
       source = "ViMaSter/allinkl"
       version = "0.1.0"
     }
+    keycloak = {
+      source = "mrparkers/keycloak"
+      version = "4.4.0"
+    }
   }
 }
 
@@ -28,4 +32,26 @@ resource "allinkl_ddns" "subdomain" {
     dyndns_zone      = "mahn.ke"
     dyndns_label     = "${local.subdomain_label}.by.vincent"
     dyndns_target_ip = "88.99.215.101"
+}
+
+provider "keycloak" {
+    url           = "https://sso.by.vincent.mahn.ke"
+}
+
+data "keycloak_realm" "sso_by_vincent_mahn_ke" {
+  realm = "sso.by.vincent.mahn.ke"
+}
+
+resource "keycloak_openid_client" "openid_client" {
+  realm_id            = data.keycloak_realm.sso_by_vincent_mahn_ke.id
+  client_id           = "ttrss"
+
+  name                = "Tiny Tiny RSS"
+  enabled             = true
+
+  access_type         = "CONFIDENTIAL"
+  valid_redirect_uris = [
+    "https://ttrss.by.vincent.mahn.ke/*"
+  ]
+  use_refresh_tokens  = false
 }
