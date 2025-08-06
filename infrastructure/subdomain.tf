2e981e88301bfa416117bdeccf0f1ddefa3e9649
Vincent Mahnke <vincent@mahn.ke>
2025-08-06 22:09:52 +0200

    chore: Relocates terraform backend and provider setup

--- infrastructure/subdomain.tf
+++ infrastructure/subdomain.tf
@@ -1,22 +1,3 @@
-terraform {
-  backend "pg" {
-  }
-  required_providers {
-    allinkl = {
-      source = "ViMaSter/allinkl"
-      version = "0.1.0"
-    }
-    keycloak = {
-      source = "mrparkers/keycloak"
-      version = "4.4.0"
-    }
-  }
-}
-
-provider "allinkl" {
-    kas_auth_type = "plain"
-}
-
 resource "random_password" "ddns_password" {
     length  = 30
     special = false
