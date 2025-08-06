867b5b767059a798179a3b5cc760f1ff6625d020
Vincent Mahnke <vincent@mahn.ke>
2025-08-06 22:26:55 +0200

    chore: Relocates OpenID client to separate file

--- infrastructure/subdomain.tf
+++ infrastructure/subdomain.tf
@@ -14,25 +14,3 @@ resource "allinkl_ddns" "subdomain" {
     dyndns_label     = "${local.subdomain_label}.by.vincent"
     dyndns_target_ip = "88.99.215.101"
 }
-
-provider "keycloak" {
-    url           = "https://sso.by.vincent.mahn.ke"
-}
-
-data "keycloak_realm" "sso_by_vincent_mahn_ke" {
-  realm = "sso.by.vincent.mahn.ke"
-}
-
-resource "keycloak_openid_client" "openid_client" {
-  realm_id            = data.keycloak_realm.sso_by_vincent_mahn_ke.id
-  client_id           = "ttrss"
-
-  name                = "Tiny Tiny RSS"
-  enabled             = true
-
-  access_type         = "CONFIDENTIAL"
-  valid_redirect_uris = [
-    "https://ttrss.by.vincent.mahn.ke/*"
-  ]
-  use_refresh_tokens  = false
-}
