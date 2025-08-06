2f85fb996d5bcda48e7700a3e982ccc5f75a96e8
Vincent Mahnke <accounts@vimaster.de>
2025-08-06 23:28:12 +0200

    chore: Relocates terraform backend and provider setup

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,22 +3,22 @@ terraform {
   }
   required_providers {
     allinkl = {
-      source = "ViMaSter/allinkl"
-      version = "0.1.0"
+      source  = "ViMaSter/allinkl"
+      version = "0.1.2"
     }
     keycloak = {
-      source = "mrparkers/keycloak"
+      source  = "mrparkers/keycloak"
       version = "4.4.0"
     }
   }
 }
 
 provider "allinkl" {
-    kas_auth_type = "plain"
+  kas_auth_type = "plain"
 }
 
 provider "keycloak" {
-    url           = "https://sso.by.vincent.mahn.ke"
+  url = "https://sso.by.vincent.mahn.ke"
 }
 
 data "keycloak_realm" "sso_by_vincent_mahn_ke" {
