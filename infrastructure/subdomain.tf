a4b634fd122bd40c75387e1f95dcf6d94c9fd966
Vincent Mahnke <github@vincent.mahn.ke>
2025-12-11 04:39:07 +0100

    fix: Changes IP

--- infrastructure/subdomain.tf
+++ infrastructure/subdomain.tf
@@ -6,5 +6,5 @@ resource "allinkl_dns" "subdomain" {
   zone_host   = "mahn.ke"
   record_type = "A"
   record_name = local.subdomain_label
-  record_data = "88.99.215.101"
+  record_data = "89.99.215.101"
 }
