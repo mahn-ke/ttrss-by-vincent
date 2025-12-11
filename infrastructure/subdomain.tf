8f92f205a99b0bae1bdc939b7e5995b76b9af51e
Vincent Mahnke <github@vincent.mahn.ke>
2025-12-11 04:39:54 +0100

    Update subdomain.tf

--- infrastructure/subdomain.tf
+++ infrastructure/subdomain.tf
@@ -6,5 +6,5 @@ resource "allinkl_dns" "subdomain" {
   zone_host   = "mahn.ke"
   record_type = "A"
   record_name = local.subdomain_label
-  record_data = "88.99.215.101"
+  record_data = "81.99.215.101"
 }
